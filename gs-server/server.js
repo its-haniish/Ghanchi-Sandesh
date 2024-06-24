1354855b88d0fba967c255cfdf60ab4c08630a7e
Hanish Kumar <142662543+its-haniish@users.noreply.github.com>
2024-06-24 16:49:33 +0530

    Update server.js

--- gs-server/server.js
+++ gs-server/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -7,11 +8,7 @@ const connectDB = require('./utils/connectDB.js')
 const routes = require('./routes/routes.js')
 
 app.use(express.json({ limit: '50mb' }));
-app.use(cors(
-    {
-        origin: "http://localhost:3000"
-    }
-));
+app.use(cors());
 app.use('/', routes);
 
 app.get("/", (req, res) => {
@@ -20,7 +17,7 @@ app.get("/", (req, res) => {
 
 
 // calling the connectDB function and listening server in then block
-connectDB("mongodb://127.0.0.1:27017/gs-database").then(() => {
+connectDB(process.env.MONGO_URI).then(() => {
     // listening server
     app.listen(PORT, () => {
         console.log('Server is running on port:', PORT);

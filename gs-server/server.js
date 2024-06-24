5680727035631073fbce18b8ab3c9dec62a96f39
Hanish Kumar <142662543+its-haniish@users.noreply.github.com>
2024-06-24 16:43:05 +0530

    Update server.js

--- gs-server/server.js
+++ gs-server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const connectDB = require('./utils/connectDB.js')
 const routes = require('./routes/routes.js')
 
